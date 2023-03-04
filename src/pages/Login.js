e7cfcbce65ae432bb5e93ccc7d96191926f067e6
seunjay <seunjay92@gmail.com>
2023-03-04 08:30:57 +0100

    basic login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,41 @@
-import React from "react";
+import axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+  //reqres registered sample user
+  const loginPayload = {
+    email: "eve.holt@reqres.in",
+    password: "cityslicka",
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        "https://reqres.in/api/login",
+        loginPayload
+      );
+
+      const token = data?.token;
+
+      console.log(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <br />
+      <input type="email" id="email" name="email" />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input type="password" id="password" name="password" />
+      <br></br>
+      <input type="submit" value="Submit" onClick={handleSubmit} />
+    </form>
+  );
 };
 
 export default Login;
