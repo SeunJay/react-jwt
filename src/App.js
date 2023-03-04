2c36fb616631c169a015bbed9f8113a005be046c
seunjay <seunjay92@gmail.com>
2023-03-04 08:20:07 +0100

    set up home and login routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="">
-     
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
